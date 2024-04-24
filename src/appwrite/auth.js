fe3d1729f80acdf8585a4004f6030e179f38fa9b
SanjanaSingh1818 <sanjanasingh63072@gmail.com>
2024-04-24 07:39:47 +0530

    first commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,55 @@
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+            
+    }
+
+    async createAccount({ email, password, name }) {
+        const userAccount = await this.account.create(ID.unique(), email, password, name);
+        if (userAccount) {
+            
+            return this.login({ email, password });
+        } else {
+            return userAccount;
+        }
+    }
+    
+
+    async login({ email, password }) {
+        return this.account.createEmailPasswordSession(email, password);
+      }
+      
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService
